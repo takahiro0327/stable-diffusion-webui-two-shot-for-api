545b7ed8f811ad71f2d2889ff09bb0a8fc8557a7
RAYTREK\takah <takahiro0327@gmail.com>
2023-12-19 21:40:15 +0900

    APIからマスク画像を設定可能にした

--- scripts/two_shot.py
+++ scripts/two_shot.py
@@ -17,6 +17,9 @@ from modules.script_callbacks import CFGDenoisedParams, on_cfg_denoised
 
 from modules.processing import StableDiffusionProcessing
 
+from io import BytesIO
+from PIL import Image
+
 MAX_COLORS = 12
 switch_values_symbol = '\U000021C5' # ⇅
 
@@ -164,7 +167,7 @@ class Script(scripts.Script):
 
 
     def title(self):
-        return "Latent Couple extension"
+        return "Latent Couple extension for API"
 
     def show(self, is_img2img):
         return scripts.AlwaysVisible
@@ -247,6 +250,117 @@ class Script(scripts.Script):
 
         return raw_params.get('divisions', '1:1,1:2,1:2'), raw_params.get('positions', '0:0,0:0,0:1'), raw_params.get('weights', '0.2,0.8,0.8'), int(raw_params.get('step', '20'))
 
+    def process_sketch(self, img_arr, input_binary_matrixes):
+        input_binary_matrixes.clear()
+        # base64_img = canvas_data['image']
+        # image_data = base64.b64decode(base64_img.split(',')[1])
+        # image = Image.open(BytesIO(image_data)).convert("RGB")
+        im2arr = img_arr
+        # colors = [tuple(map(int, rgb[4:-1].split(','))) for rgb in
+        #           ['colors']]
+        sketch_colors, color_counts = np.unique(im2arr.reshape(-1, im2arr.shape[2]), axis=0, return_counts=True)
+        colors_fixed = []
+        # if color count is less than 0.001 of total pixel count, collect it for edge color correction
+        edge_color_correction_arr = []
+        for sketch_color_idx, color in enumerate(sketch_colors[:-1]):  # exclude white
+            if color_counts[sketch_color_idx] < im2arr.shape[0] * im2arr.shape[1] * 0.002:
+                edge_color_correction_arr.append(sketch_color_idx)
+
+        edge_fix_dict = {}
+        # TODO:for every non area color pixel in img_arr, find the nearest area color pixel and replace it with that color
+
+        area_colors = np.delete(sketch_colors, edge_color_correction_arr, axis=0)
+        if self.mask_denoise:
+            for edge_color_idx in edge_color_correction_arr:
+                edge_color = sketch_colors[edge_color_idx]
+                # find the nearest area_color
+
+                color_distances = np.linalg.norm(area_colors - edge_color, axis=1)
+                nearest_index = np.argmin(color_distances)
+                nearest_color = area_colors[nearest_index]
+                edge_fix_dict[edge_color_idx] = nearest_color
+                # replace edge color with the nearest area_color
+                cur_color_mask = np.all(im2arr == edge_color, axis=2)
+                im2arr[cur_color_mask] = nearest_color
+
+            # recalculate area colors
+            sketch_colors, color_counts = np.unique(im2arr.reshape(-1, im2arr.shape[2]), axis=0, return_counts=True)
+            area_colors = sketch_colors
+
+        # create binary matrix for each area_color
+        area_color_maps = []
+        self.ndmasks = []
+        self.area_colors = area_colors
+        for color in area_colors:
+            r, g, b = color
+            mask, binary_matrix = create_binary_matrix_base64(im2arr, color)
+            self.ndmasks.append(mask)
+            input_binary_matrixes.append(binary_matrix)
+            colors_fixed.append(gr.update(
+                value=f'<div style="display:flex;justify-content:center;max-height: 94px;"><img width="20%" style="object-fit: contain;flex-grow:1;margin-right: 1em;" src="data:image/png;base64,{binary_matrix}" /><div class="color-bg-item" style="background-color: rgb({r},{g},{b});width:10%;height:auto;"></div></div>'))
+
+
+
+        visibilities = []
+        sketch_colors = []
+
+        for sketch_color_idx in range(MAX_COLORS):
+            visibilities.append(gr.update(visible=False))
+            sketch_colors.append(gr.update(value=f'<div class="color-bg-item" style="background-color: black"></div>'))
+        for j in range(len(colors_fixed)-1):
+            visibilities[j] = gr.update(visible=True)
+            sketch_colors[j] = colors_fixed[j]
+
+        alpha_mask_visibility = gr.update(visible=True)
+        alpha_mask_html = colors_fixed[-1]
+        return [gr.update(visible=True), input_binary_matrixes, alpha_mask_visibility, alpha_mask_html, *visibilities, *sketch_colors]
+
+    def update_mask_filters(self, alpha_blend_val, general_prompt_str, *cur_weights_and_prompts):
+        cur_weight_slider_vals = cur_weights_and_prompts[:MAX_COLORS]
+        cur_prompts = cur_weights_and_prompts[MAX_COLORS:]
+        general_mask = self.ndmasks[-1]
+        final_filter_list = []
+        for m in range(len(self.ndmasks) - 1):
+            cur_float_mask = self.ndmasks[m].astype(np.float32) * float(cur_weight_slider_vals[m]) * float(1.0-alpha_blend_val)
+            mask_filter = MaskFilter(float_mask=cur_float_mask)
+            final_filter_list.append(mask_filter)
+        # subtract the sum of all masks from the general mask to get the alpha blend mask
+        initial_general_mask = np.ones(shape=general_mask.shape, dtype=np.float32)
+        alpha_blend_mask = initial_general_mask.astype(np.float32) - np.sum([f.mask for f in final_filter_list], axis=0)
+        alpha_blend_filter = MaskFilter(float_mask=alpha_blend_mask)
+        final_filter_list.insert(0, alpha_blend_filter)
+        self.filters = final_filter_list
+
+
+        sketch_colors = []
+        colors_fixed = []
+        for area_idx, color in enumerate(self.area_colors):
+            r, g, b = color
+            final_list_idx = area_idx + 1
+            if final_list_idx == len(final_filter_list):
+                final_list_idx = 0
+            # get shape of current mask
+            height_b, width_b = final_filter_list[final_list_idx].mask.shape
+            current_mask = torch.nn.functional.interpolate(final_filter_list[final_list_idx].tensor_mask.unsqueeze(0).unsqueeze(0),
+                                                    size=(int(height_b/8), int(width_b/8)), mode='nearest-exact').squeeze(0).squeeze(0).cpu().numpy()
+            adjusted_mask = current_mask * 255
+            _, adjusted_mask_arr = cv2.imencode('.png', adjusted_mask)
+
+            adjusted_mask_b64 = base64.b64encode(adjusted_mask_arr.tobytes()).decode('ascii')
+            colors_fixed.append(gr.update(
+                value=f'<div style="display:flex;justify-content:center;max-height: 94px;"><img width="20%" style="object-fit: contain;flex-grow:1;margin-right: 1em;" src="data:image/png;base64,{adjusted_mask_b64}" /><div class="color-bg-item" style="background-color: rgb({r},{g},{b});width:10%;height:auto;"></div></div>'))
+        for sketch_color_idx in range(MAX_COLORS):
+
+            sketch_colors.append(
+                gr.update(value=f'<div class="color-bg-item" style="background-color: black"></div>'))
+        for j in range(len(colors_fixed)-1):
+
+            sketch_colors[j] = colors_fixed[j]
+        alpha_mask_visibility = gr.update(visible=True)
+        alpha_mask_html = colors_fixed[-1]
+        final_prompt_update = gr.update(value='\nAND '.join([general_prompt_str, *cur_prompts[:len(colors_fixed)-1]]))
+        return [final_prompt_update, alpha_mask_visibility, alpha_mask_html, *sketch_colors]
+    
     def ui(self, is_img2img):
         process_script_params = []
         id_part = "img2img" if is_img2img else "txt2img"
@@ -261,116 +375,6 @@ class Script(scripts.Script):
         def create_canvas(h, w):
             return np.zeros(shape=(h, w, 3), dtype=np.uint8) + 255
 
-        def process_sketch(img_arr, input_binary_matrixes):
-            input_binary_matrixes.clear()
-            # base64_img = canvas_data['image']
-            # image_data = base64.b64decode(base64_img.split(',')[1])
-            # image = Image.open(BytesIO(image_data)).convert("RGB")
-            im2arr = img_arr
-            # colors = [tuple(map(int, rgb[4:-1].split(','))) for rgb in
-            #           ['colors']]
-            sketch_colors, color_counts = np.unique(im2arr.reshape(-1, im2arr.shape[2]), axis=0, return_counts=True)
-            colors_fixed = []
-            # if color count is less than 0.001 of total pixel count, collect it for edge color correction
-            edge_color_correction_arr = []
-            for sketch_color_idx, color in enumerate(sketch_colors[:-1]):  # exclude white
-                if color_counts[sketch_color_idx] < im2arr.shape[0] * im2arr.shape[1] * 0.002:
-                    edge_color_correction_arr.append(sketch_color_idx)
-
-            edge_fix_dict = {}
-            # TODO:for every non area color pixel in img_arr, find the nearest area color pixel and replace it with that color
-
-            area_colors = np.delete(sketch_colors, edge_color_correction_arr, axis=0)
-            if self.mask_denoise:
-                for edge_color_idx in edge_color_correction_arr:
-                    edge_color = sketch_colors[edge_color_idx]
-                    # find the nearest area_color
-
-                    color_distances = np.linalg.norm(area_colors - edge_color, axis=1)
-                    nearest_index = np.argmin(color_distances)
-                    nearest_color = area_colors[nearest_index]
-                    edge_fix_dict[edge_color_idx] = nearest_color
-                    # replace edge color with the nearest area_color
-                    cur_color_mask = np.all(im2arr == edge_color, axis=2)
-                    im2arr[cur_color_mask] = nearest_color
-
-                # recalculate area colors
-                sketch_colors, color_counts = np.unique(im2arr.reshape(-1, im2arr.shape[2]), axis=0, return_counts=True)
-                area_colors = sketch_colors
-
-            # create binary matrix for each area_color
-            area_color_maps = []
-            self.ndmasks = []
-            self.area_colors = area_colors
-            for color in area_colors:
-                r, g, b = color
-                mask, binary_matrix = create_binary_matrix_base64(im2arr, color)
-                self.ndmasks.append(mask)
-                input_binary_matrixes.append(binary_matrix)
-                colors_fixed.append(gr.update(
-                    value=f'<div style="display:flex;justify-content:center;max-height: 94px;"><img width="20%" style="object-fit: contain;flex-grow:1;margin-right: 1em;" src="data:image/png;base64,{binary_matrix}" /><div class="color-bg-item" style="background-color: rgb({r},{g},{b});width:10%;height:auto;"></div></div>'))
-
-
-
-            visibilities = []
-            sketch_colors = []
-
-            for sketch_color_idx in range(MAX_COLORS):
-                visibilities.append(gr.update(visible=False))
-                sketch_colors.append(gr.update(value=f'<div class="color-bg-item" style="background-color: black"></div>'))
-            for j in range(len(colors_fixed)-1):
-                visibilities[j] = gr.update(visible=True)
-                sketch_colors[j] = colors_fixed[j]
-
-            alpha_mask_visibility = gr.update(visible=True)
-            alpha_mask_html = colors_fixed[-1]
-            return [gr.update(visible=True), input_binary_matrixes, alpha_mask_visibility, alpha_mask_html, *visibilities, *sketch_colors]
-
-        def update_mask_filters(alpha_blend_val, general_prompt_str, *cur_weights_and_prompts):
-            cur_weight_slider_vals = cur_weights_and_prompts[:MAX_COLORS]
-            cur_prompts = cur_weights_and_prompts[MAX_COLORS:]
-            general_mask = self.ndmasks[-1]
-            final_filter_list = []
-            for m in range(len(self.ndmasks) - 1):
-                cur_float_mask = self.ndmasks[m].astype(np.float32) * float(cur_weight_slider_vals[m]) * float(1.0-alpha_blend_val)
-                mask_filter = MaskFilter(float_mask=cur_float_mask)
-                final_filter_list.append(mask_filter)
-            # subtract the sum of all masks from the general mask to get the alpha blend mask
-            initial_general_mask = np.ones(shape=general_mask.shape, dtype=np.float32)
-            alpha_blend_mask = initial_general_mask.astype(np.float32) - np.sum([f.mask for f in final_filter_list], axis=0)
-            alpha_blend_filter = MaskFilter(float_mask=alpha_blend_mask)
-            final_filter_list.insert(0, alpha_blend_filter)
-            self.filters = final_filter_list
-
-
-            sketch_colors = []
-            colors_fixed = []
-            for area_idx, color in enumerate(self.area_colors):
-                r, g, b = color
-                final_list_idx = area_idx + 1
-                if final_list_idx == len(final_filter_list):
-                    final_list_idx = 0
-                # get shape of current mask
-                height_b, width_b = final_filter_list[final_list_idx].mask.shape
-                current_mask = torch.nn.functional.interpolate(final_filter_list[final_list_idx].tensor_mask.unsqueeze(0).unsqueeze(0),
-                                                       size=(int(height_b/8), int(width_b/8)), mode='nearest-exact').squeeze(0).squeeze(0).cpu().numpy()
-                adjusted_mask = current_mask * 255
-                _, adjusted_mask_arr = cv2.imencode('.png', adjusted_mask)
-
-                adjusted_mask_b64 = base64.b64encode(adjusted_mask_arr.tobytes()).decode('ascii')
-                colors_fixed.append(gr.update(
-                    value=f'<div style="display:flex;justify-content:center;max-height: 94px;"><img width="20%" style="object-fit: contain;flex-grow:1;margin-right: 1em;" src="data:image/png;base64,{adjusted_mask_b64}" /><div class="color-bg-item" style="background-color: rgb({r},{g},{b});width:10%;height:auto;"></div></div>'))
-            for sketch_color_idx in range(MAX_COLORS):
-
-                sketch_colors.append(
-                    gr.update(value=f'<div class="color-bg-item" style="background-color: black"></div>'))
-            for j in range(len(colors_fixed)-1):
-
-                sketch_colors[j] = colors_fixed[j]
-            alpha_mask_visibility = gr.update(visible=True)
-            alpha_mask_html = colors_fixed[-1]
-            final_prompt_update = gr.update(value='\nAND '.join([general_prompt_str, *cur_prompts[:len(colors_fixed)-1]]))
-            return [final_prompt_update, alpha_mask_visibility, alpha_mask_html, *sketch_colors]
 
 
 
@@ -432,11 +436,11 @@ class Script(scripts.Script):
                             button_update = gr.Button("Prompt Info Update", elem_id="update_button", interactive=True)
                             final_prompt = gr.Textbox(label="Final Prompt", interactive=False)
 
-                        button_run.click(process_sketch, inputs=[canvas_image, binary_matrixes],
+                        button_run.click(self.process_sketch, inputs=[canvas_image, binary_matrixes],
                                          outputs=[post_sketch, binary_matrixes, alpha_mask_row, alpha_color, *color_row, *colors],
                                          queue=False)
 
-                        button_update.click(fn=update_mask_filters, inputs=[alpha_blend, general_prompt, *cur_weight_sliders, *prompts], outputs=[final_prompt, alpha_mask_row, alpha_color, *colors])
+                        button_update.click(fn=self.update_mask_filters, inputs=[alpha_blend, general_prompt, *cur_weight_sliders, *prompts], outputs=[final_prompt, alpha_mask_row, alpha_color, *colors])
 
                         def paste_prompt(*input_prompts):
                             final_prompts = input_prompts[:len(self.area_colors)]
@@ -498,6 +502,7 @@ class Script(scripts.Script):
         process_script_params.append(weights)
         process_script_params.append(end_at_step)
         process_script_params.append(alpha_blend)
+        process_script_params.append(enabled)               #ダミー
         process_script_params.extend(cur_weight_sliders)
         return process_script_params
 
@@ -554,12 +559,23 @@ class Script(scripts.Script):
 
     def process(self, p: StableDiffusionProcessing, *args, **kwargs):
 
-        enabled, raw_divisions, raw_positions, raw_weights, raw_end_at_step, alpha_blend, *cur_weight_sliders = args
+        enabled, raw_divisions, raw_positions, raw_weights, raw_end_at_step, alpha_blend, base64_img, *cur_weight_sliders = args
 
         self.enabled = enabled
 
         if not self.enabled:
             return
+                
+        if type(base64_img) is str:
+            image_data = base64.b64decode(base64_img)
+            image = Image.open(BytesIO(image_data)).convert("RGB")
+            image = np.array(image)
+            
+            binary_matrixes = []
+            self.process_sketch(image, binary_matrixes)
+            weight_and_dummy_prompt = cur_weight_sliders + [""] * MAX_COLORS
+            self.update_mask_filters(alpha_blend, "", *weight_and_dummy_prompt)
+
 
         self.num_batches = p.batch_size
 
